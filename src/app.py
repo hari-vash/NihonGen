e7301347ea73a58d7d8f7570f6b7dfedf04fcf10
Hariansh Vashisht <harianshvashisht24155@gmail.com>
2026-08-08 12:42:30 +0530

    updated app.py to output a polished text

--- src/app.py
+++ src/app.py
@@ -6,17 +6,25 @@ from langchain.agents import create_agent
 from langchain.messages import HumanMessage
 from kanji_class import KanjiFormat
 from system_prompt import system_prompt
+import time
+import sys
 
 model = ChatGoogleGenerativeAI(model="gemini-3.5-flash-lite")
 
 agent = create_agent(
     model=model,
     system_prompt=system_prompt,
-    format=KanjiFormat,
+    response_format=KanjiFormat,
 )
 
 response = agent.invoke(
     {"messages": HumanMessage(content="新")}
 )
 
-print(response['messages'][-1].content[0]["text"])
+parsed_response = response["structured_response"]
+polished_text = parsed_response.to_polished_string()
+
+for char in polished_text:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.015)
